0f5184c1c463957a825c37146cd9ef08a0b24064
Andrey Melnik <andreymelnik@MacBook-Pro-Andrey.local>
2023-09-22 07:38:02 +0300

    update

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-import data from './russia_geojson_wgs84.geojson' assert {type: 'json'};
+// import data from './russia_geojson_wgs84.geojson' assert {type: 'json'};
 import stations from './stations.json' assert {type: 'json'};
 import lines from './lines.json' assert {type: 'json'};
 import linePoints from './linePoints.json' assert {type: 'json'};
 
 window.onload = async function() {
+    const showOts = document.querySelector('#ots')
 
     const corner1 = L.latLng(82.273831, 12)
     const corner2 = L.latLng(37.151806, 177.99319)
     const bounds = L.latLngBounds(corner1, corner2)
 
-    let map = L.map('map', {zoomSnap: 0.5,zoomDelta: 0.5, wheelPxPerZoomLevel: 180, attributionControl: false, maxBounds: bounds});
-    map.setView([55.66401, 37.052989], 5);
+    const key = '9uMou4WBAcqERBnW7CQj';
+     const map = L.map('map', {
+        zoomSnap: 0.5,
+        zoomDelta: 0.5,
+        wheelPxPerZoomLevel: 180,
+        attributionControl: true,
+        scrollWheelZoom: false, // disable original zoom function
+        smoothWheelZoom: true,  // enable smooth zoom
+        smoothSensitivity: 4,
+        maxBounds: bounds
+    })
+    map.setView([52.289588, 104.280606], 5);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.maptilerLayer({
+        apiKey: key,
+        style: "97153cbc-9568-4fe0-8863-bfc30bd5cd18", //optional
         noWrap: true,
-        minZoom: 5,
-        maxZoom: 14,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
     // Russia map
-    await L.geoJSON(data, {
-        style: function (feature) {
-            return {color: feature.properties.color};
-        }
-    }).addTo(map);
+    // await L.geoJSON(data, {
+    //     style: function (feature) {
+    //         return {color: feature.properties.color};
+    //     }
+    // }).addTo(map);
 
     lines.forEach(line => {
         line.nodes = line.nodes.map(item => {
@@ -34,16 +44,44 @@ window.onload = async function() {
     })
 
     lines.forEach(line => {
-        L.polyline(line.nodes, {zIndex: 10, color: '#2b5ad2'}).addTo(map);
+        L.polyline(line.nodes, {color: '#2b5ad2'}).addTo(map);
     })
 
     const sortedStations = stations.filter((obj, index, self) =>
         index === self.findIndex((o) => o.tags.name === obj.tags.name)
     );
 
+    const allStations = []
     sortedStations.forEach((item, idx) => {
         let name = L.divIcon({className: 'station-text', html: item.tags.name});
-        L.circleMarker([item.lat,item.lon], {zIndex: 10, fillColor: '#2b5ad2', color: '#a1e4fa', weight: 1, fillOpacity: 1, radius: 5}).addTo(map);
-        L.marker([item.lat,item.lon], {icon: name}).addTo(map);
+        const stationIcon = L.circleMarker([item.lat,item.lon], {fillColor: '#2b5ad2', color: '#a1e4fa', weight: 1, fillOpacity: 1, radius: 5});
+        const stationName = L.marker([item.lat,item.lon], {icon: name})
+        const stationGroup = L.featureGroup([stationIcon,stationName]).addTo(map)
+        allStations.push(stationName)
+    })
+
+    const stationsLayer = L.layerGroup(allStations).addTo(map)
+
+    showStations(5)
+    function showStations(zoom) {
+        if (zoom > 10) {
+            stationsLayer.eachLayer(function(layer) {
+                layer.setOpacity(1);
+            });
+        } else {
+            stationsLayer.eachLayer(function(layer) {
+                layer.setOpacity(0);
+            });
+        }
+    }
+
+    map.on('zoomend', () => {
+        showStations(map.getZoom())
+    })
+
+    showOts.addEventListener('click', () => {
+        lines.forEach(line => {
+            L.polyline(line.nodes, {color: '#f3c71b'}).addTo(map);
+        })
     })
 }
